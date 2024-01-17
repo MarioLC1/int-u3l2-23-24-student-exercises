7e5fe8a3175b59dd080131a00ad70f1ffa5cfeb5
MarioLC1 <135175010+MarioLC1@users.noreply.github.com>
2024-01-17 14:51:45 +0000

    a

--- storingElements.js
+++ storingElements.js
@@ -1,24 +1,28 @@
 // CODE ALONG
 // 1. Create a variable called `painting1` that stores the html element with the class `painting-1` using querySelector.
-
+const painting1 = document.querySelector(".painting-1");
 
 
 // CODE SOLO
 // 2. Create a variable called `painting2` that stores the html element with the class `painting-2` using querySelector.
-
+const painting2 = document.querySelector(".painting-2");
 // 3. Create a variable called `painting3` that stores the html element with the class `painting-3` using querySelector.
-
+const painting3 = document.querySelector(".painting-3");
 // 4. Create a variable called `painting4` that stores the html element with the class `painting-4` using querySelector.
-
+const painting4 = document.querySelector(".painting-4");
 // 5. Create a variable called `title` that stores the title tag with using querySelector.
-
-
-
+const title = document.querySelector("h1");
+const painting6 = document.querySelector(".painting-6");
+const painting5 = document.querySelector(".painting-5");
 
 
 // 6. Once tasks 1-5 are complete, uncomment the code below.
-// painting1.style.width = "500px";
-// painting2.style.animation = "mynewmove 10s infinite";
-// painting3.style.animation = "dance 10s infinite";
-// painting4.style.border = " 10px solid #0fd623";
-// title.innerHTML = "MUHAHAHA!";
+painting1.style.width = "2000px";
+painting1.style.animation = "dance 0.5s infinite alternate";
+painting2.style.animation = "mynewmove 0.50s infinite";
+painting3.style.animation = "dance 0.25s infinite";
+painting4.style.border = " 10px solid #953553";
+painting4.style.animation = "dance 0.01s infinite";
+title.innerHTML = "VIRUS!";
+painting6.style.animation = "dance 0.1s infinite alternate";
+painting5.style.animation = "dance 0.1s infinite";
